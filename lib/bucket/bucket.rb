ef70e4ef737f4b8569166a95ec75a4fef336d6e0
CodeByBlazej <codebyblazej@gmail.com>
2024-11-30 13:39:26 +0000

    modify add_node method

--- lib/bucket/bucket.rb
+++ lib/bucket/bucket.rb
@@ -9,8 +9,22 @@ class Bucket
     @tail = nil
   end
 
+  def add_node(key, value)
+    if @head.nil?
+      @head = Node.new(key, value)
+    else
+      current_node = @head
+      new_node = Node.new(key, value)
+
+      while current_node.next_node != nil
+        current_node = current_node.next_node
+      end
+      current_node.next_node = new_node
+    end
+  end
+
   def to_s
     arr = [@key, @value]
-    puts arr
+    p arr
   end
 end
