fc76c269fc88d0a5a70901235422767d91ef4d0b
CodeByBlazej <codebyblazej@gmail.com>
2024-11-30 19:58:55 +0000

    test program

--- main.rb
+++ main.rb
@@ -5,5 +5,9 @@ test = HashMap.new(0.75)
 test.set('apple', 'red')
 test.set('banana', 'yellow')
 test.set('apple', 'orange')
+test.set('apple', 'black')
+test.set('Carlos', 'im the old value')
+test.set('watermelon', 'im the new value')
+
 
 puts test
