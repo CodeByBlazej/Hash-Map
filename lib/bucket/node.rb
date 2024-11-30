7a6783924811b84bb37eb6b976632c79883b957c
CodeByBlazej <codebyblazej@gmail.com>
2024-11-30 13:39:48 +0000

    modify node method

--- lib/bucket/node.rb
+++ lib/bucket/node.rb
@@ -1,9 +1,14 @@
 class Node
-  attr_accessor :key, value
-  
+  attr_accessor :key, :value
+
   def initialize(key, value)
     @next_node = nil
     @key = key
     @value = value
+  end
+
+  def to_s
+    displayed_value = [key, value]
+    puts displayed_value
   end
 end
