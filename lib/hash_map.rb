ab946c294793b222a6eb87acd193b5af5daf55c1
CodeByBlazej <codebyblazej@gmail.com>
2024-11-30 13:38:56 +0000

    modify set method

--- lib/hash_map.rb
+++ lib/hash_map.rb
@@ -1,10 +1,15 @@
+require_relative "bucket/bucket"
+require_relative "bucket/node"
+require "pry-byebug"
+
 class HashMap
-  attr_accessor :load_factor, :capacity
+  attr_accessor :load_factor, :capacity, :hash_code
   
-  def initialize(load_factor, capacity)
+  def initialize(load_factor)
     @load_factor = load_factor
-    @capacity = capacity
+    @capacity = 16
     @buckets = []
+    @hash_code = nil
   end
 
   def hash(key)
@@ -13,18 +18,28 @@ class HashMap
 
     key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
 
-    hash_code
+    @hash_code = hash_code % @capacity
+    # hash_code = @hash_code
   end
 
   def set(key, value)
-    hash_code = hash(key)
-    index = hash_code
-    new_bucket = Bucket.new(index, key, value)
+    @hash_code = hash(key)
+    index = @hash_code
+    # raise IndexError if index.negative? || index >= @buckets.length
+    # binding.pry
+    current_bucket = @buckets[index]
 
-    if @buckets[index] != nil
-      @buckets[index] = new_bucket
+    if current_bucket != nil
+      current_bucket.add_node(key, value)
     end
 
+    new_bucket = Bucket.new(index)
+    new_bucket.add_node(key, value)
     @buckets[index] = new_bucket
+  end
+
+  def to_s
+    p @buckets
+  
   end
 end
